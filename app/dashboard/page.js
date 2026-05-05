ae16fdbe50de75039099b63d8bc3a16ce7bdf7d0
phil <soccerphil8@yahoo.com>
2026-05-05 05:26:34 -0500

    -- Moved DashboardInventoryItems to accounts/page -- accounts/page.js:    - removed "use client"    - added function to calculate total progress to get overall percentage    - added items/snippets to show the users goals on the accounts page -- app/dashboard    - added displaying of users pantry if said item is larger than 0 -- app/onboarding    - changed height placeholder to inches -- app/pantry    - fully connected pantry to the database -- app/api/auth/register    - new users now have all their pantry items set to 0 -- app/api/pantry    - created to help route pantry calls and updates (should do this to rest post full release) -- app/api/user/onboard    - added macro goal initialization for users based on onboarding details -- app/api/user    - new api for user calls -- schema.prisma    - update height to be numeric for inches    - added weekly budget for meals    - added prepTimeGoal for users

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -7,7 +7,6 @@ import RecipeSuggestions from "../components/dashboard/RecipeSuggestions";
 import styles from "./dashboard.module.css";
 import { getAuthUser } from "@/lib/auth";
 import {
-  dashboardInventoryItems,
   dashboardRecipes,
 } from "../components/dashboard/dashboardContent";
 
@@ -25,6 +24,57 @@ export default async function DashboardPage() {
     { title: "Carbs", current: user.car_Prog, goal: user.car_Goal, unit: "g" },
     { title: "Fats", current: user.fat_Prog, goal: user.fat_Goal, unit: "g" },
   ];
+
+  // { id: 101, name: "Whole Milk", quantity: getDbItem("Whole Milk").quantity, measurement: getDbItem("Whole Milk").unit },
+  // { id: 102, name: "Large Eggs", quantity: getDbItem("Large Eggs").quantity, measurement: getDbItem("Large Eggs").unit },
+  // { id: 103, name: "Roma Tomatoes", quantity: getDbItem("Roma Tomatoes").quantity, measurement: getDbItem("Roma Tomatoes").unit },
+  // { id: 104, name: "Ground Beef", quantity: getDbItem("Ground Beef").quantity, measurement: getDbItem("Ground Beef").unit },
+  // { id: 105, name: "Onions", quantity: getDbItem("Onions").quantity, measurement: getDbItem("Onions").unit },
+  // { id: 106, name: "Garlic", quantity: getDbItem("Garlic").quantity, measurement: getDbItem("Garlic").unit },
+  // { id: 107, name: "Butter", quantity: getDbItem("Butter").quantity, measurement: getDbItem("Butter").unit },
+  // { id: 108, name: "Salt", quantity: getDbItem("Salt").quantity, measurement: getDbItem("Salt").unit },
+
+  const dashboardInventoryItems = [
+
+  ];
+
+  const getDbItem = (name) =>
+      user.userPantry.find((i) => i.itemName === name) || { quantity: 0, unit: "Unit" };
+
+  //This is so cancerous, I hate it but we are running out of time
+
+  if(getDbItem("Whole Milk").quantity > 0)
+  {
+    dashboardInventoryItems.push("Whole Milk")
+  }
+  if(getDbItem("Large Eggs").quantity > 0)
+  {
+    dashboardInventoryItems.push("Large Eggs")
+  }
+  if(getDbItem("Roma Tomatoes").quantity > 0)
+  {
+    dashboardInventoryItems.push("Roma Tomatoes")
+  }
+  if(getDbItem("Ground Beef").quantity > 0)
+  {
+    dashboardInventoryItems.push("Ground Beef")
+  }
+  if(getDbItem("Onions").quantity > 0)
+  {
+    dashboardInventoryItems.push("Onions")
+  }
+  if(getDbItem("Garlic").quantity > 0)
+  {
+    dashboardInventoryItems.push("Garlic")
+  }
+  if(getDbItem("Butter").quantity > 0)
+  {
+    dashboardInventoryItems.push("Butter")
+  }
+  if(getDbItem("Salt").quantity > 0)
+  {
+    dashboardInventoryItems.push("Salt")
+  }
 
   return (
     <div className={styles.pageStack}>
